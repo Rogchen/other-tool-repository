d086f460b434fe6571e8eb2bf1386adeba61c186
rogchen <chenhk128@163.com>
2019-09-18 13:24:15 +0800

    modify design

---
+++ demo/src/main/java/com/ylzinfo/ms/testDate.java
@@ -0,0 +1,10 @@
+public class testDate {
+	
+	
+	public static String main(String []args){
+		System.out.println(newDate());
+		System.out.println(System.getProperty("user.timezone"));
+		
+	}
+	
+}
---
+++ http-client/src/main/java/com/ylzinfo/ms/utils/OkHttp/HebeiTest.java
@@ -0,0 +1,10 @@
+package com.ylzinfo.ms.utils.OkHttp;
+
+/**
+ * @Description:
+ * @Product: IntelliJ IDEA
+ * @Author Rogchen rogchen128@gmail.com
+ * @Created Date: 2019/7/24 15:22
+ **/
+public class HebeiTest {
+}
